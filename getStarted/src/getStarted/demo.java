b9509349187b8a8519e156065e9880ba5c8adc6d
gthac <gthac@10.144.117.219>
2024-09-24 15:57:13 -0700

    changed test commit

--- getStarted/src/getStarted/demo.java
+++ getStarted/src/getStarted/demo.java
@@ -3,6 +3,6 @@ package getStarted;
 public class demo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to CSC 305");
+		System.out.println("Welcome to CSC 305, its gonna be a blast");
 	}
 }
